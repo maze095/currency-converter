34bc797ed5969cdd30d6e9aaae3df1755b01b142
maze095 <maze89@mail.ru>
2017-12-14 21:36:35 +0300

    1я версия кода на JS

---
+++ 1-Currency Converter.js
@@ -0,0 +1,7 @@
+	
+	var rub =  parseInt(prompt("Введите сумму в рублях: "));
+	var RUBtoUSD = rub*58.84;
+	var RUBtoEUR = rub*69.24;
+
+	alert("RUB to USD: " + RUBtoUSD, 
+		  "RUB to EUR: " + RUBtoEUR);
