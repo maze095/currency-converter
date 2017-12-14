1fd90a7ec0d6ba72d7ef9644cfd1c90fc804c1f2
maze095 <maze89@mail.ru>
2017-12-14 21:50:51 +0300

    Добавлен цикл, реализующий проверку на ввод числа

--- 1-Currency Converter.js
+++ 1-Currency Converter.js
@@ -1,7 +1,12 @@
 	
-	var rub =  parseInt(prompt("Введите сумму в рублях: "));
+	var rub;
 	var RUBtoUSD = rub*58.84;
 	var RUBtoEUR = rub*69.24;
+
+	do 	{
+    	  rub  = prompt('Введите сумму в рублях:', '0');
+		} 
+	while (isNaN(rub));
 
 	alert("RUB to USD: " + RUBtoUSD, 
 		  "RUB to EUR: " + RUBtoEUR);
